54d53e2a1e1c068973a0d09302e49adf8ec5f7d1
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-11-18 15:39:59 +0100

    delete account

--- backend/public/account-table.js
+++ backend/public/account-table.js
@@ -12,9 +12,22 @@ function createElement(elementType, parent, content) {
 function createTableHeader(table) {
   const thead = createElement('thead', table);
   const theadRow = createElement('tr', thead);
-  const headings = ['Vorname', 'Nachname', 'Username', 'Alter'];
+  const headings = ['Vorname', 'Nachname', 'Username', 'Alter', 'löschen'];
   for (const heading of headings) {
     createElement('th', theadRow, heading);
+  }
+}
+
+async function deleteAccount(id) {
+  const response = await fetch('http://localhost:8080/accounts/' + id, {
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    // zeile löschen
+    document.querySelector(`[data-id="${id}"]`).remove();
+    console.log('deleted successfully');
+  } else {
+    console.error('failed');
   }
 }
 
@@ -22,10 +35,16 @@ function createTableBody(table, data) {
   const tbody = createElement('tbody', table);
   for (const account of data) {
     const tr = createElement('tr', tbody);
+    tr.dataset.id = account.id;
     createElement('td', tr, account.firstName);
     createElement('td', tr, account.lastName);
     createElement('td', tr, account.userName);
     createElement('td', tr, account.age);
+    const buttonCell = createElement('td', tr);
+    const button = createElement('button', buttonCell, 'löschen');
+    button.addEventListener('click', () => {
+      deleteAccount(account.id);
+    });
   }
 }
 
